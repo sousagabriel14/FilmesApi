5eadccfd7319f8d60ee41a1396223562c10c478c
sousagabriel14 <gbsousa.dev@gmail.com>
2026-04-29 18:00:28 -0300

    Criando e recebendo os dados de um filme

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,21 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+
+    }
+
+}
